6927a9f940e221f1c7f6c3e2c432727d813d724b
Marc Vizcaino <68388418+marcvizca@users.noreply.github.com>
2024-11-13 16:07:29 +0100

    Update URL google sheet

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("subscribeForm").addEventListener("submit", function(eve
     const responseMessage = document.getElementById("responseMessage");
 
     // Realiza la solicitud POST al endpoint de Google Apps Script
-    fetch("https://script.google.com/macros/s/AKfycbxx9lmd-DhQRoFBXdQeqiwxfZbWpUzUKvNQnFSRjG99GyWcdxIlIGNkNCzbJIRcw5wL/exec", {
+    fetch("https://script.google.com/macros/s/AKfycbyGcpLtC_f2FcbhDZJUNDGCRZ-M-bW6FPgHqyVzLqB34ZyCQVytxzI7Ihvc89xJJoFTyQ/exec", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: email })
