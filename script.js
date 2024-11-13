5e038581f58134f72f3908040ea24cbc536262c6
Marc Vizcaino <68388418+marcvizca@users.noreply.github.com>
2024-11-13 15:14:50 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("subscribeForm").addEventListener("submit", function(event) {
+    event.preventDefault();  // Evita el envío del formulario de forma tradicional
+
+    const email = event.target.email.value;
+    const responseMessage = document.getElementById("responseMessage");
+
+    // Realiza la solicitud POST al endpoint de Google Apps Script
+    fetch("https://script.google.com/macros/s/AKfycbyFbzndeWsy1_5plUIGE8TKeY6kxoHWKwQOvrizlySSGeiJd3mN6tNNy0taeBuaAFyxUw/exec", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email: email })
+    })
+    .then(response => {
+        if (response.ok) {
+            responseMessage.textContent = "¡Gracias por suscribirte!";
+            responseMessage.style.color = "green";
+            event.target.reset();  // Limpia el formulario
+        } else {
+            responseMessage.textContent = "Hubo un problema. Inténtalo de nuevo.";
+            responseMessage.style.color = "red";
+        }
+    })
+    .catch(error => {
+        responseMessage.textContent = "Error de red. Intenta más tarde.";
+        responseMessage.style.color = "red";
+        console.error("Error:", error);
+    });
+});
