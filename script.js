16631ca85b20d8a81db9c6c85ca62fb5e47887d9
Marc Vizcaino <68388418+marcvizca@users.noreply.github.com>
2024-11-13 16:22:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("subscribeForm").addEventListener("submit", function(eve
     const responseMessage = document.getElementById("responseMessage");
 
     // Realiza la solicitud POST al endpoint de Google Apps Script
-    fetch("https://script.google.com/macros/s/AKfycbyGcpLtC_f2FcbhDZJUNDGCRZ-M-bW6FPgHqyVzLqB34ZyCQVytxzI7Ihvc89xJJoFTyQ/exec", {
+    fetch("https://script.google.com/macros/s/AKfycbyVJCFzknOnYyRYI8X8K_r-n9IaGwCBwD0SGgeo1B9ZayFSS-olUD5oB-dUYr15mQkk/exec", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: email })
