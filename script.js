d428d61a543329ea9fd4abfa8a6e814d462dc60b
Marc Vizcaino <68388418+marcvizca@users.noreply.github.com>
2024-11-13 18:03:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ document.getElementById("subscribeForm").addEventListener("submit", function(eve
     const responseMessage = document.getElementById("responseMessage");
 
     // Realiza la solicitud POST al endpoint de Google Apps Script
-    fetch("https://script.google.com/macros/s/AKfycbxPeqZ6yF5WagMxJdngGwUGgLPsDJJMrRZMKCHG5c4Pyx0hLyGzLHn_CDynwdlksodhpw/exec?email=" + encodeURIComponent(email), {
+    fetch("https://script.google.com/macros/s/AKfycbxUguhUYhl8t34aMZp_L_wk2jPTfsMnHSg2fufjjTBB9oyNFhX7r-YTORHpz-ha1DU3iQ/exec?email=" + encodeURIComponent(email), {
         method: "GET",
     })
     .then(response => {
+        console.log(response);
         if (response.ok) {
             responseMessage.textContent = "¡Gracias por suscribirte!";
             responseMessage.style.color = "green";
